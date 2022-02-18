04a5d92cb82b2a8f4c57f96289d013f753a9d41a
Valerio Vaz <valerio@focusmedia.pt>
2022-02-18 23:48:30 +0000

    updated bio

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
-console.log('Valerio Vaz');
+console.log(`
+Valerio Vaz
+
+Is a WordPress frontend developer. Works as a Maintenance Team Lead at Valet.io and teaches open-source stuff at courses.valeriovaz.com
+
+Twitter: https://twitter.com/vvaz
+Github:  https://github.com/vvaz
+Blog:    https://valeriovaz.dev
+Email:   mail@valeriovaz.dev
+
+`);
