04b935599c2d56b6fc55d0e9060a984b59e67df2
vvaz <valerio@wpbox.io>
2022-02-23 22:38:42 +0000

    change url

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ clearConsole();
 console.log(`
 Valerio Vaz
 
-Is a WordPress frontend developer. Works as a Maintenance Team Lead at Valet.io and teaches open-source stuff at courses.valeriovaz.com
+Is a WordPress frontend developer. Works as a Maintenance Team Lead at Valet.io and teaches open-source stuff at courses.valeriovaz.dev
 
 Twitter: https://twitter.com/vvaz
 Github:  https://github.com/vvaz
